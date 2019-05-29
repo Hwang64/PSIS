490b3c1161bf9f957340c3d63d404595d45387c6
Hwang64 <37236874+Hwang64@users.noreply.github.com>
2019-05-29 15:32:50 +0800

    Add files via upload

---
+++ class_imbalance_loss/roi_weighted_layer.py
@@ -0,0 +1,248 @@
+# --------------------------------------------------------
+# Fast R-CNN
+# Copyright (c) 2015 Microsoft
+# Licensed under The MIT License [see LICENSE for details]
+# Written by Ross Girshick
+# --------------------------------------------------------
+
+"""The data layer used during training to train a Fast R-CNN network.
+
+RoIDataLayer implements a Caffe Python layer.
+"""
+
+import caffe
+from fast_rcnn.config import cfg
+from roi_data_layer.minibatch import get_minibatch
+import numpy as np
+import yaml
+from multiprocessing import Process, Queue
+import json
+import os
+class RoIWeightedDataLayer(caffe.Layer):
+    """Fast R-CNN data layer used for training."""
+
+    def _shuffle_roidb_inds(self, gpu_id=0):
+        self.gpu_id = gpu_id
+        """Randomly permute the training roidb."""
+        if cfg.TRAIN.ASPECT_GROUPING:
+            widths = np.array([r['width'] for r in self._roidb])
+            heights = np.array([r['height'] for r in self._roidb])
+            horz = (widths >= heights)
+            vert = np.logical_not(horz)
+            horz_inds = np.where(horz)[0]
+            vert_inds = np.where(vert)[0]
+            inds = np.hstack((
+                np.random.permutation(horz_inds),
+                np.random.permutation(vert_inds)))
+            inds = np.reshape(inds, (-1, 2))
+            np.random.seed(gpu_id)
+            row_perm = np.random.permutation(np.arange(inds.shape[0]))
+            inds = np.reshape(inds[row_perm, :], (-1,))
+            self._perm = inds
+        else:
+            self._perm = np.random.permutation(np.arange(len(self._roidb)))
+        self._cur = 0
+
+    def _get_next_minibatch_inds(self):
+        """Return the roidb indices for the next minibatch."""
+        if self._cur + cfg.TRAIN.IMS_PER_BATCH >= len(self._roidb):
+            self._shuffle_roidb_inds(self.gpu_id)
+
+        db_inds = self._perm[self._cur:self._cur + cfg.TRAIN.IMS_PER_BATCH]
+        self._cur += cfg.TRAIN.IMS_PER_BATCH
+        return db_inds
+
+    def _get_next_minibatch(self):
+        """Return the blobs to be used for the next minibatch.
+
+        If cfg.TRAIN.USE_PREFETCH is True, then blobs will be computed in a
+        separate process and made available through self._blob_queue.
+        """
+        if cfg.TRAIN.USE_PREFETCH:
+            return self._blob_queue.get()
+        else:
+            db_inds = self._get_next_minibatch_inds()
+            minibatch_db = [self._roidb[i] for i in db_inds]
+            return get_minibatch(minibatch_db, self._num_classes,self._mean)
+
+    def _class_number(self):
+        '''
+        calculated each class weight in coco dataset
+        '''
+        object_list=[]
+        ap_list=[]
+        category_list=[]
+        total_num = 0.0
+        cls_num=np.zeros((1,81))
+        fjson_src = open('/home/wh/py-faster-rcnn_test/data/coco/annotations/instances_train2017.json', 'r')
+        dataset_src = json.load(fjson_src)
+        with open(os.path.join('/home/wh/py-faster-rcnn_mask/lib/roi_data_layer/','category.txt')) as f_cls:
+            for cls_line in f_cls.readlines():
+                cls_line=cls_line.strip("\n")
+                object_list.append(cls_line)
+                ap_list.append(0.0)
+        for i in xrange(len(dataset_src['categories'])):
+            category_list.append(dataset_src['categories'][i]['id'])
+        ob2ap = dict(zip(object_list,ap_list))
+        id2name = dict(zip(category_list,object_list))
+        annotations = {annotation['id']:annotation for annotation in dataset_src['annotations']}
+        annkeys = annotations.keys()
+        for annkey in annkeys:
+            class_id=annotations[annkey]['category_id']
+            class_name=id2name[class_id]
+            ob2ap[class_name]=ob2ap[class_name]+1
+            total_num=total_num+1
+        ap_list=[]
+        for category in object_list:
+            print 'Class %20.16s number is %8d ratio is %6f'%(category,ob2ap[category],ob2ap[category]/total_num)
+            #ob2ap[category] = ob2ap[category]/total_num
+            ap_list.append(ob2ap[category])
+        for i in xrange(len(ap_list)):
+            cls_num[0][i+1]=ap_list[i]
+        cls_num[0][0]=total_num*3/80 # background equal 3 times with foreground
+        #blobs['cls_weight'] = cls_weight
+        return cls_num
+
+    def set_roidb(self, roidb, gpu_id=0):
+        """Set the roidb to be used by this layer during training."""
+        self._roidb = roidb
+        self._shuffle_roidb_inds(gpu_id)
+        self._cls_num=self._class_number()
+        if cfg.TRAIN.USE_PREFETCH:
+            self._blob_queue = Queue(10)
+            self._prefetch_process = BlobFetcher(self._blob_queue,
+                                                 self._roidb,
+                                                 self._num_classes, gpu_id)
+            self._prefetch_process.start()
+            # Terminate the child process when the parent exists
+            def cleanup():
+                print 'Terminating BlobFetcher'
+                self._prefetch_process.terminate()
+                self._prefetch_process.join()
+            import atexit
+            atexit.register(cleanup)
+
+    def setup(self, bottom, top):
+        """Setup the RoIDataLayer."""
+
+        # parse the layer parameter string, which must be valid YAML
+        layer_params = yaml.load(self.param_str_)
+
+        self._num_classes = layer_params['num_classes']
+        self._beta = layer_params['beta']
+        self._mean = 0
+
+        self._name_to_top_map = {}
+
+        # data blob: holds a batch of N images, each with 3 channels
+        idx = 0
+        top[idx].reshape(cfg.TRAIN.IMS_PER_BATCH, 3,
+            max(cfg.TRAIN.SCALES), cfg.TRAIN.MAX_SIZE)
+        self._name_to_top_map['data'] = idx
+        idx += 1
+
+        if cfg.TRAIN.HAS_RPN:
+            top[idx].reshape(1, 3)
+            self._name_to_top_map['im_info'] = idx
+            idx += 1
+
+            top[idx].reshape(1, 4)
+            self._name_to_top_map['gt_boxes'] = idx
+            idx += 1
+
+            top[idx].reshape(1,81)
+            self._name_to_top_map['cls_weight'] = idx
+            idx += 1
+        else: # not using RPN
+            # rois blob: holds R regions of interest, each is a 5-tuple
+            # (n, x1, y1, x2, y2) specifying an image batch index n and a
+            # rectangle (x1, y1, x2, y2)
+            top[idx].reshape(1, 5, 1, 1)
+            self._name_to_top_map['rois'] = idx
+            idx += 1
+
+            # labels blob: R categorical labels in [0, ..., K] for K foreground
+            # classes plus background
+            top[idx].reshape(1, 1, 1, 1)
+            self._name_to_top_map['labels'] = idx
+            idx += 1
+
+            if cfg.TRAIN.BBOX_REG:
+                # bbox_targets blob: R bounding-box regression targets with 4
+                # targets per class
+                num_reg_class = 2 if cfg.TRAIN.AGNOSTIC else self._num_classes
+
+                top[idx].reshape(1, num_reg_class * 4, 1, 1)
+                self._name_to_top_map['bbox_targets'] = idx
+                idx += 1
+
+                # bbox_inside_weights blob: At most 4 targets per roi are active;
+                # thisbinary vector sepcifies the subset of active targets
+                top[idx].reshape(1, num_reg_class * 4, 1, 1)
+                self._name_to_top_map['bbox_inside_weights'] = idx
+                idx += 1
+
+                top[idx].reshape(1, num_reg_class * 4, 1, 1)
+                self._name_to_top_map['bbox_outside_weights'] = idx
+                idx += 1
+
+        print 'RoiDataLayer: name_to_top:', self._name_to_top_map
+        assert len(top) == len(self._name_to_top_map)
+
+    def forward(self, bottom, top):
+        """Get blobs and copy them into this layer's top blob vector."""
+        blobs = self._get_next_minibatch()
+        effective_num = 1.0 - np.power(self._beta,self._cls_num)
+        cls_weights=(1-self._beta) / effective_num 
+        cls_weights=cls_weights/np.sum(cls_weights)*int(self._num_classes)
+        blobs['cls_weight'] = cls_weights
+        for blob_name, blob in blobs.iteritems():
+            top_ind = self._name_to_top_map[blob_name]
+            top[top_ind].reshape(*(blob.shape))
+            # Copy data into net's input blobs
+            top[top_ind].data[...] = blob.astype(np.float32, copy=False)
+
+    def backward(self, top, propagate_down, bottom):
+        """This layer does not propagate gradients."""
+        pass
+
+    def reshape(self, bottom, top):
+        """Reshaping happens during the call to forward."""
+        pass
+
+class BlobFetcher(Process):
+    """Experimental class for prefetching blobs in a separate process."""
+    def __init__(self, queue, roidb, num_classes, gpu_id=0):
+        super(BlobFetcher, self).__init__()
+        self._queue = queue
+        self._roidb = roidb
+        self._num_classes = num_classes
+        self._perm = None
+        self._cur = 0
+        self.gpu_id = gpu_id
+        np.random.seed(gpu_id)
+        self._shuffle_roidb_inds()
+
+    def _shuffle_roidb_inds(self):
+        """Randomly permute the training roidb."""
+        # TODO(rbg): remove duplicated code
+        self._perm = np.random.permutation(np.arange(len(self._roidb)))
+        self._cur = 0
+
+    def _get_next_minibatch_inds(self):
+        """Return the roidb indices for the next minibatch."""
+        # TODO(rbg): remove duplicated code
+        if self._cur + cfg.TRAIN.IMS_PER_BATCH >= len(self._roidb):
+            self._shuffle_roidb_inds()
+
+        db_inds = self._perm[self._cur:self._cur + cfg.TRAIN.IMS_PER_BATCH]
+        self._cur += cfg.TRAIN.IMS_PER_BATCH
+        return db_inds
+
+    def run(self):
+        print 'BlobFetcher started'
+        while True:
+            db_inds = self._get_next_minibatch_inds()
+            minibatch_db = [self._roidb[i] for i in db_inds]
+            blobs = get_minibatch(minibatch_db, self._num_classes)
+            self._queue.put(blobs)
